5ac6c5f72565470fe7a50f30963104c9b5a8abad
Laurent <laurent@laurentroger.com>
2024-06-09 17:37:35 +0200

    0.0.8 : SDK51, use getLocales

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import I18n from './vendor/i18n';
-import * as Localization from 'expo-localization'
+import { getLocales } from 'expo-localization';
 
 I18n.initAsync = async () => {
-  const locale = await Localization.locale;
-  I18n.locale = (locale) ? locale.replace(/_/, '-') : '';
-}
+  const locale = getLocales();
+  I18n.locale = locale ? locale.replace(/_/, '-') : '';
+};
 
 export default I18n;
